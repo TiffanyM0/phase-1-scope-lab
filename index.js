a465f49f1604f181f9a30f674ec018f239518204
TIFFANY MAINA <tiffmaina@gmail.com>
2023-10-10 17:01:45 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob"
+var bestCustomer;
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  return bestCustomer = 'not bob';
+}
+
+console.log(bestCustomer);
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = '';
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'lancey';
+}
